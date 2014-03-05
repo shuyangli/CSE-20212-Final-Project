866f110147aefc899a8bdca03fc913249ab4b72f
Shuyang Li <shuyang.li.95@gmail.com>
2014-03-05 11:42:46 -0500

    initial commit from xcode

---
+++ GroupProjectCSE20212/GroupProjectCSE20212/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GroupProjectCSE20212
+//
+//  Created by Shuyang Li on 03/05/14.
+//  Copyright (c) 2014 Shuyang Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
