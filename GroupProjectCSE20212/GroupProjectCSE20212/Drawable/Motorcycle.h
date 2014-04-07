b2c1837325deceed4786283b03143e4a025cd198
Yuxuan "Ethan" Chen <ychen18@nd.edu>
2014-04-07 09:37:24 -0400

    Defined the Motorcycle class.

--- GroupProjectCSE20212/GroupProjectCSE20212/Drawable/Motorcycle.h
+++ GroupProjectCSE20212/GroupProjectCSE20212/Drawable/Motorcycle.h
@@ -10,5 +10,33 @@
 #define __GroupProjectCSE20212__Motorcycle__
 
 #include <iostream>
+#include "Drawable.h"
+
+class Motorcycle : Drawable {
+    
+public:
+    
+    Motorcycle() { }
+    ~Motorcycle() { }
+    
+    void draw(); // Draws the motorcycle
+    void move(); // Moves the motorcycle
+    int isInBounds();           // Determines if the motorcycle is in on the road
+    int isInCollision();        // Determines if the motorcycle collides with something
+    
+    // Getters and Setters
+    void setSpeed(double);
+    double getSpeed() const;
+    void setPosition(double);
+    double getPosition() const;
+    
+    int hasCollision();
+    
+private:
+    
+    double speed;    // Speed of the motorcycle
+    double position; // Position on the road, range from 0 to the width of the road
+    
+};
 
 #endif /* defined(__GroupProjectCSE20212__Motorcycle__) */
