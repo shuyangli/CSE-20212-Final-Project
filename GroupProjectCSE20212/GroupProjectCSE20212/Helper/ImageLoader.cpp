78776f8ee4ffe1e86ddc8f608d6ef8d5d7e847f9
Shuyang Li <shuyang.li.95@gmail.com>
2014-04-27 22:00:03 -0400

    image loader

--- GroupProjectCSE20212/GroupProjectCSE20212/Helper/ImageLoader.cpp
+++ GroupProjectCSE20212/GroupProjectCSE20212/Helper/ImageLoader.cpp
@@ -7,8 +7,59 @@
 //
 
 #include "ImageLoader.h"
-#include <CoreFoundation/CoreFoundation.h>
-#include <CoreGraphics/CoreGraphics.h>
+#include "Constants.h"
 
+GLuint ImageLoader::loadImageAsTexture() {
+    
+    GLuint textureHandle = -1;
+    
+    // generate texture
+    glGenTextures(1, &textureHandle);
+    glBindTexture(GL_TEXTURE_2D, textureHandle);
+    
+    // we're not using mipmaps
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // actually loading image
+    CGDataProviderRef imageDataProvider = CGDataProviderCreateWithFilename(SKYBOX_TEXTURE_PATH);
+    CGImageRef image = CGImageCreateWithJPEGDataProvider(imageDataProvider,
+                                                         NULL,
+                                                         false,
+                                                         kCGRenderingIntentDefault);
+    // setup parameters
+    GLuint imageWidth = (GLuint) CGImageGetWidth(image);
+    GLuint imageHeight = (GLuint) CGImageGetHeight(image);
 
-
+    // create context
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    void * imageData = malloc(imageWidth * imageHeight * 4);
+    CGContextRef context = CGBitmapContextCreate(imageData, imageWidth, imageHeight,
+                                                 8, 4 * imageWidth, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Host);
+    CGColorSpaceRelease(colorSpace);
+    
+    // draw image into context
+    CGContextClearRect(context, CGRectMake(0, 0, imageWidth, imageHeight));
+    CGContextTranslateCTM(context, 0, 0);
+    CGContextDrawImage(context, CGRectMake(0, 0, imageWidth, imageHeight), image);
+    
+    // use image data from context as texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+                 imageWidth, imageHeight,
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE, imageData);
+    
+    // unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+    
+    // clean up
+    CGContextRelease(context);
+    CGImageRelease(image);
+    CGDataProviderRelease(imageDataProvider);
+    free(imageData);
+    
+    // return handle
+    return textureHandle;
+}
--- GroupProjectCSE20212/GroupProjectCSE20212/Helper/ImageLoader.h
+++ GroupProjectCSE20212/GroupProjectCSE20212/Helper/ImageLoader.h
@@ -10,15 +10,17 @@
 #define __GroupProjectCSE20212__ImageLoader__
 
 #include <iostream>
+#include <CoreGraphics/CoreGraphics.h>
 
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+
+
+// caveat: CGImage has the y axis inverted!
 class ImageLoader {
     
 public:
-    ImageLoader();
-    ~ImageLoader();
-    
-    unsigned char loadImage();
-    
+    static GLuint loadImageAsTexture();
     
 };
 
